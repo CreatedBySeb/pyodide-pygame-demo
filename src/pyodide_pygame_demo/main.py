d0ee8b65a082926162d101bd7a66ecc8a499b2e9
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2025-12-20 18:00:51 +0000

    Add bounce sound effect

--- src/pyodide_pygame_demo/main.py
+++ src/pyodide_pygame_demo/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import importlib.resources
 import pygame
 
 BALL_RADIUS = 40
@@ -18,10 +19,18 @@ def wrap(value: int, max: int) -> int:
 async def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
+
     running = True
+
     ball_colour = pygame.Color("blue")
     ball_direction = pygame.Vector2(1, 1)
     ball_position = pygame.Vector2(*SCREEN_CENTRE)
+
+    bounce_sound = pygame.Sound(
+        importlib.resources.files().joinpath("assets", "blip.wav")
+    )
+    bounce_sound.set_volume(0.25)
+
     last_ticks = pygame.time.get_ticks()
     dt = 0.0
 
@@ -41,20 +50,31 @@ async def main():
         # and time since last frame
         ball_position += ball_direction * BALL_SPEED * dt
 
+        # Track if the ball bounced to determine whether to play the sound
+        bounced = False
+
         # Bounce the ball if it hits the left or right sides
         if ball_position.x - BALL_RADIUS < 0:
             ball_direction.x = 1
+            bounced = True
         elif ball_position.x + BALL_RADIUS > SCREEN_SIZE[0]:
             ball_direction.x = -1
+            bounced = True
 
         # Bounce the ball if it hits the top or bottom sides
         if ball_position.y - BALL_RADIUS < 0:
             ball_direction.y = 1
+            bounced = True
         elif ball_position.y + BALL_RADIUS > SCREEN_SIZE[1]:
             ball_direction.y = -1
+            bounced = True
 
+        # Play the sound effect if we bounced off of any edge
+        if bounced:
+            bounce_sound.play()
+
+        # Draw the ball and flip the framebuffer
         pygame.draw.circle(screen, ball_colour, ball_position, BALL_RADIUS)
-
         pygame.display.flip()
 
         # Replacement for framerate-less pygame.Clock.tick
